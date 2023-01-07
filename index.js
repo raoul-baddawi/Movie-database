6363b473edc744340b2e6fcf22560d7b7841271f
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-01-07 12:01:37 +0200

    step 5

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (response) => {
-  response.send('ok');
-});
+app.get('/', (req, res) => {
+    res.send('ok');
+  });
 
-app.listen(2999, () => {
-  console.log('Server listening on port 2999');
+app.listen(3002, () => {
+  console.log('Server listening on port 3002');
 });
 
 app.get('/test', (response) => {
@@ -21,16 +21,45 @@ app.get('/time', (response) => {
   response.json({ status: 200, message: `${hours}:${minutes}` });
 });
 
-app.get('/hello/:id', (request, response) => {
-    const { id } = request.params;
-    response.json({ status: 200, message: `Hello, ${id}` });
+app.get('/hello/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ status: 200, message: `Hello, ${id}` });
   });
   
-  app.get('/search', (request, response) => {
-    const { string } = request.query;
+  app.get('/search', (req, res) => {
+    const { string } = req.query;
     if (string) {
-      response.json({ status: 200, message: 'ok', data: string });
+      res.json({ status: 200, message: 'ok', data: s });
     } else {
-      response.status(500).json({ status: 500, error: true, message: 'you have to provide a search' });
+      res.status(500).json({ status: 500, error: true, message: 'you have to provide a search' });
     }
   });
+
+  const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+app.get('/movies/create/:id', (req, res) => {
+    const movieId = req.params.id;
+      res.send(`Movie ${movieId} created!`);
+    });
+  
+
+    app.get('/movies/read/', (req, res) => {
+      res.json({ status: 200, data: movies});
+    });
+    
+
+    app.get('/movies/update/:id', (req, res) => {
+      const movieId = req.params.id;
+      res.send(`Movie ${movieId} updated!`);
+    });
+    
+
+    app.get('/movies/delete/:id', (req, res) => {
+      const movieId = req.params.id;
+      res.send(`Movie ${movieId} deleted!`);
+    });
