fb95906268f6a0acc90ab047cdde7579c83c5145
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-01-07 11:20:33 +0200

    step 3

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('working on port 3000');
+app.get('/', (response) => {
+  response.send('ok');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(2999, () => {
+  console.log('Server listening on port 2999');
+});
+
+app.get('/test', (response) => {
+  response.json({ status: 200, message: 'ok' });
+});
+
+app.get('/time', (response) => {
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  response.json({ status: 200, message: `${hours}:${minutes}` });
 });
