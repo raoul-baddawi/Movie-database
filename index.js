f97b844fed26b8740d41143858ecf23037672dec
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-01-07 10:52:07 +0200

    step 2

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('working on port 3000');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
