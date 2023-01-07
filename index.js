d7dc117be8c1d0687307325109a05dee19a96ee8
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-01-07 11:29:11 +0200

    step 4

--- index.js
+++ index.js
@@ -20,3 +20,17 @@ app.get('/time', (response) => {
   const minutes = currentTime.getMinutes();
   response.json({ status: 200, message: `${hours}:${minutes}` });
 });
+
+app.get('/hello/:id', (request, response) => {
+    const { id } = request.params;
+    response.json({ status: 200, message: `Hello, ${id}` });
+  });
+  
+  app.get('/search', (request, response) => {
+    const { string } = request.query;
+    if (string) {
+      response.json({ status: 200, message: 'ok', data: string });
+    } else {
+      response.status(500).json({ status: 500, error: true, message: 'you have to provide a search' });
+    }
+  });
